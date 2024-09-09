523ad05e8cdba0df3d881eba005f6a836d52fae2
김보경 <135104493+aishahansten@users.noreply.github.com>
2024-09-09 18:03:58 +0900

    [Gold III] Title: 싸지방에 간 준하, Time: 504 ms, Memory: 62660 KB -BaekjoonHub

--- 백준/Gold/12764. 싸지방에 간 준하/싸지방에 간 준하.py
+++ 백준/Gold/12764. 싸지방에 간 준하/싸지방에 간 준하.py
@@ -1,33 +1,22 @@
-import sys
+import heapq, sys
 input = sys.stdin.readline
-import heapq
-
-N = int(input())
-
-X = 0
-hq = []
-cnt = [0]*(N)
-current_status_of_computers = [0]*N
-for _ in range(N):
-    P, Q = map(int, input().split())
-    heapq.heappush(hq, (P,Q))
-
-# print(hq) [(10, 100), (20, 50), (30, 120), (60, 110), (80, 90)]
-
-while hq:
-    now = heapq.heappop(hq)
-    for i in range(len(current_status_of_computers)):
-        if current_status_of_computers[i] <= now[0]:
-            if current_status_of_computers[i] == 0:
-                X+=1
-            current_status_of_computers[i] = now[1]
-            cnt[i] += 1
-            break
-    
-
-print(X)
-for j in cnt:
-    if j == 0:
-        pass
-    else:
-        print(j, end=' ')
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+arr.sort(key = lambda x: x[0])
+minH = []
+seat = []
+for i in range(1, n+1):
+    heapq.heappush(seat, i)
+count = [0 for _ in range(n+1)]
+max_count = 0
+for x in arr:
+    while minH and x[0] >= minH[0][0]:
+        end_time, s = heapq.heappop(minH)
+        heapq.heappush(seat, s)
+    s = heapq.heappop(seat)
+    heapq.heappush(minH, [x[1], s])
+    max_count = max(max_count, len(minH))
+    count[s] += 1
+print(max_count)
+for i in range(1, max_count+1):
+    print(count[i], end=' ')
